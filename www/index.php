ce7e64db4224c35c06751400685d565a5181f181
omarpachuco <omarpachuco>
2022-10-13 22:45:32 -0500

    C/Variables

---
+++ www/index.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        <title>Welcome to LAMP Infrastructure</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+        <a href="light/index.html">Dashboard</a>
+            <?php
+                echo "<h1>Hola Bayer</h1>";
+
+                $conn = mysqli_connect('db', 'root', 'alfil', "dbCuento");
+
+                $query = 'SELECT * From Cuento';
+                $result = mysqli_query($conn, $query);
+
+                echo '<table class="table table-striped">';
+                echo '<thead><tr><th></th><th>id</th><th>name</th></tr></thead>';
+                while($value = $result->fetch_array(MYSQLI_ASSOC)){
+                    echo '<tr>';
+                    echo '<td><a href="#"><span class="glyphicon glyphicon-search"></span></a></td>';
+                    foreach($value as $element){
+                        echo '<td>' . $element . '</td>';
+                    }
+
+                    echo '</tr>';
+                }
+                echo '</table>';
+
+                $result->close();
+                mysqli_close($conn);
+            ?>
+            
+        </div>
+    </body>
+</html>
